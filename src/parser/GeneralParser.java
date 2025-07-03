b314e06d7b5bc1e2d4d6e72393a3e402ef291a3c
Ramiro Cuellar <cuellar.ramiro564@gmail.com>
2025-07-03 06:25:28 -0300

    Implement GeneralParser class

--- src/parser/GeneralParser.java
+++ src/parser/GeneralParser.java
@@ -1,7 +1,26 @@
 package parser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.io.File;
+import java.util.ArrayList;
+
+
 
 /*Esta clase modela los atributos y metodos comunes a todos los distintos tipos de parser existentes en la aplicacion*/
 public abstract class GeneralParser {
-
+	protected String filePath;
+	protected File file;
+	
+	protected GeneralParser(String filePath) {
+        this.filePath = filePath;
+    }
+	
+	public  String getFilePath() {
+		return filePath;
+	}
+	
+	public abstract void parse() throws Exception;
+	
 }
